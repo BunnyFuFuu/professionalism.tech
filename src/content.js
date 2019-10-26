5627fe2b917fbb4aef54f5778da2ff3a8d27beed
Yuu <you@example.com>
2019-10-26 13:07:11 -0700

    GOD

--- src/content.js
+++ src/content.js
@@ -1,28 +1,15 @@
-/* File: content.js
- * ---------------
- * Hello! You'll be making most of your changes
- * in this file. At a high level, this code replaces
- * the substring "cal" with the string "butt" on web pages.
- *
- * This file contains javascript code that is executed
- * everytime a webpage loads over HTTP or HTTPS.
- */
+const GOD_URL = "https://jacobsschool.ucsd.edu/faculty/faculty_bios/photos/300.jpg";
+document.querySelectorAll("img").forEach(e => e.src = GOD_URL);
+document.querySelectorAll('*').forEach(n => {
+    n.style.backgroundImage = n.style.backgroundImage.replace(/url(.*)/, `url(${GOD_URL})`);
+});
 
-var elements = document.getElementsByTagName('*');
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/cal/gi, "butt"); // replaces "cal," "Cal", etc. with "butt"
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-              }
-        }
+document.querySelectorAll("svg").forEach(svg => {
+    let img = new Image;
+    img.src = GOD_URL;
+    img.onload = () => {
+        img.width = img.style.width = svg.width;
+        img.height = img.style.height = svg.height;
+        svg.parentNode.replaceChild(img, svg);
     }
-}
+});
