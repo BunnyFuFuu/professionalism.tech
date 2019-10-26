d38963740869e7254f69354e9fac784ef42d6c89
BunnyFuFuu <aliang@ucsd.edu>
2019-10-26 12:26:07 -0700

    Added basic gary memery

--- src/content.js
+++ src/content.js
@@ -18,7 +18,39 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/cal/gi, "butt"); // replaces "cal," "Cal", etc. with "butt"
+            var replacedText = text.replace(/Gary Gillespie/gi, "Big Boi");
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+              }
+        }
+    }
+}
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/Gillespie/gi, "Almighty");
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+              }
+        }
+    }
+}
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/Gary/gi, "the Lord");
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
