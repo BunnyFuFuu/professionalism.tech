ef37c6c81171e12671fb39ad4628749b1276ddd4
Yuu <you@example.com>
2019-10-26 17:12:23 -0700

    Added request logging

--- src/servers/api/routers/sentiment/index.js
+++ src/servers/api/routers/sentiment/index.js
@@ -5,16 +5,22 @@ const { execSync } = require("child_process");
 module.exports = function(req, res) {
     /** @type {string} */
     let text = req.query.text;
+    if (text)
+        text = text.replace(/"/g, "\\\"").trim();
 
     if (!text)
         // Bad request
         return void res.sendStatus(400);
 
     let command = `aws comprehend detect-sentiment --region us-west-2 ` + 
-                  `--language-code "en" --text "${text.replace(/"/g, "\\\"")}"`;
+                  `--language-code "en" --text "${text}"`;
 
+    let start = Date.now();
     if (process.platform != "win32") command = "sudo " + command;
     let result = execSync(command);
+
+    console.log(`Requesting text ${text}, ` + 
+                `time elapsed: ${((Date.now() - start) / 1000).toFixed(2)}s`);
 
     try {
         let parsed = JSON.parse(result);
